2c9d80e3fbda55b76043744e37e3390c93a53c9a
cchernn <cchernn@gmail.com>
2024-05-03 00:01:52 +0800

    add charts to ExpenditureSummary

--- src/Expenditure/ExpenditureSummary.js
+++ src/Expenditure/ExpenditureSummary.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react"
 import { useParams } from "react-router-dom"
-import API from "../Services/API"   
+import API from "../Services/API"
+import ChartPie from "../Services/ChartPie"
+import ExpenditureSummaryTable from "./ExpenditureSummaryTable"
 
 class ExpenditureSummaryClass extends Component {
     constructor(props) {
@@ -9,6 +11,8 @@ class ExpenditureSummaryClass extends Component {
         this.state ={
             expenditureSummary: {},
             groupId: groupId,
+            categoryIndex: "",
+            userIndex: "",
             error: "",
         }
     }
@@ -26,8 +30,11 @@ class ExpenditureSummaryClass extends Component {
             {}
         )
         .then((res) => {
-            this.setState({ expenditureSummary: res.data })
-            console.log(this.state.expenditureSummary)
+            this.setState({ 
+                expenditureSummary: res.data,
+                categoryIndex: res.data.expenditure_by_category[0].category,
+                userIndex: res.data.expenditure_by_user[0].user
+            })
         })
         .catch((err) => {
             console.log("error", err)
@@ -39,6 +46,14 @@ class ExpenditureSummaryClass extends Component {
         window.location.href = `/expenditures/${groupId}/list`
     }
 
+    handleSelectIndex = (index, dimension) => {
+        if (dimension === "category") {
+            this.setState({ categoryIndex: index }) 
+        } else if (dimension === "user") {
+            this.setState({ userIndex: index })
+        }
+    }
+
     render() {
         return (
             <section id="expenditure-summary-box" className="container">
@@ -47,11 +62,11 @@ class ExpenditureSummaryClass extends Component {
                     <button type="button" onClick={this.handleView}>List</button>
                 </div>
                 <div className="expenditure-summary-cells">
-                    <section className="expenditure-summary-cell">
-                        <div className="expenditure-summary-cell-header">
+                    <section className="expenditure-summary-value-cell">
+                        <div className="expenditure-summary-value-cell-header">
                             <h3>Expenditure Total</h3>
                         </div>
-                        <div className="expenditure-summary-cell-content">
+                        <div className="expenditure-summary-value-cell-content">
                             { this.state.expenditureSummary.expenditure_total ? (
                                     <p>{this.state.expenditureSummary.expenditure_total.total_spend.toLocaleString()}</p>
                                 ) : (
@@ -59,7 +74,7 @@ class ExpenditureSummaryClass extends Component {
                                 )
                             }
                         </div>
-                        <div className="expenditure-summary-cell-footer">
+                        <div className="expenditure-summary-value-cell-footer">
                             { this.state.expenditureSummary.expenditure_total ? (
                                     <p>{this.state.expenditureSummary.expenditure_total.total_items} transactions</p>
                                 ) : (
@@ -68,11 +83,11 @@ class ExpenditureSummaryClass extends Component {
                             }
                         </div>
                     </section>
-                    <section className="expenditure-summary-cell">
-                        <div className="expenditure-summary-cell-header">
+                    <section className="expenditure-summary-value-cell">
+                        <div className="expenditure-summary-value-cell-header">
                             <h3>Expenditure MTD</h3>
                         </div>
-                        <div className="expenditure-summary-cell-content">
+                        <div className="expenditure-summary-value-cell-content">
                             { this.state.expenditureSummary.expenditure_mtd ? (
                                     <p>{this.state.expenditureSummary.expenditure_mtd.total_spend.toLocaleString()}</p>
                                 ) : (
@@ -80,7 +95,7 @@ class ExpenditureSummaryClass extends Component {
                                 )
                             }
                         </div>
-                        <div className="expenditure-summary-cell-footer">
+                        <div className="expenditure-summary-value-cell-footer">
                             { this.state.expenditureSummary.expenditure_mtd ? (
                                     <p>{this.state.expenditureSummary.expenditure_mtd.total_items} transactions</p>
                                 ) : (
@@ -89,11 +104,11 @@ class ExpenditureSummaryClass extends Component {
                             }
                         </div>
                     </section>
-                    <section className="expenditure-summary-cell">
-                        <div className="expenditure-summary-cell-header">
+                    <section className="expenditure-summary-value-cell">
+                        <div className="expenditure-summary-value-cell-header">
                             <h3>Expenditure YTD</h3>
                         </div>
-                        <div className="expenditure-summary-cell-content">
+                        <div className="expenditure-summary-value-cell-content">
                             { this.state.expenditureSummary.expenditure_ytd ? (
                                     <p>{this.state.expenditureSummary.expenditure_ytd.total_spend.toLocaleString()}</p>
                                 ) : (
@@ -101,7 +116,7 @@ class ExpenditureSummaryClass extends Component {
                                 )
                             }
                         </div>
-                        <div className="expenditure-summary-cell-footer">
+                        <div className="expenditure-summary-value-cell-footer">
                             { this.state.expenditureSummary.expenditure_ytd ? (
                                     <p>{this.state.expenditureSummary.expenditure_ytd.total_items} transactions</p>
                                 ) : (
@@ -109,6 +124,44 @@ class ExpenditureSummaryClass extends Component {
                                 )
                             }
                         </div>
+                    </section>
+                </div>
+                <div className="expenditure-summary-chart-cells">
+                    <section className="expenditure-summary-chart-cell">
+                        <div className="expenditure-summary-chart-cell-header">
+                            <h3>Expenditure By Category</h3>
+                        </div>
+                        <ChartPie
+                            data={this.state.expenditureSummary.expenditure_by_category}
+                            dataKey="total_spend"
+                            groupKey="category"
+                            onIndexChange={this.handleSelectIndex}
+                        />
+                    </section>
+                    <section className="expenditure-summary-chart-cell">
+                        <ExpenditureSummaryTable 
+                            title="Expenditure by Category"
+                            dimension={this.state.categoryIndex}
+                            data={this.state.expenditureSummary.expenditure_by_category_breakdown}
+                        />
+                    </section>
+                    <section className="expenditure-summary-chart-cell">
+                        <div className="expenditure-summary-chart-cell-header">
+                            <h3>Expenditure By User</h3>
+                        </div>
+                        <ChartPie
+                            data={this.state.expenditureSummary.expenditure_by_user}
+                            dataKey="total_spend"
+                            groupKey="user"
+                            onIndexChange={this.handleSelectIndex}
+                        />
+                    </section>
+                    <section className="expenditure-summary-chart-cell">
+                        <ExpenditureSummaryTable 
+                            title="Expenditure by User"
+                            dimension={this.state.userIndex}
+                            data={this.state.expenditureSummary.expenditure_by_user_breakdown}
+                        />
                     </section>
                 </div>
             </section>
